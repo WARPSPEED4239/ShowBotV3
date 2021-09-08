79076bd63a729844e5889fb2f93e4e204154b191
Darren Seubert <dpseubert15@gmail.com>
2021-09-07 23:28:49 -0500

    Created stand alone commands and split Cannon into Cannon and Cannon Revolve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,48 +1,38 @@
 package frc.robot;
 
-import java.util.Map;
-
 import com.ctre.phoenix.CANifier;
 
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
-import edu.wpi.first.wpilibj2.command.SelectCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.CannonAimSetPercentOutputWithController;
-import frc.robot.commands.CannonFiringSolenoidSetState;
-import frc.robot.commands.CannonLoadingSolenoidSetState;
-import frc.robot.commands.CannonRevolve;
+import frc.robot.commands.CannonFire;
+import frc.robot.commands.CannonReloading;
+import frc.robot.commands.CannonRevolveSetPercentOutput;
+import frc.robot.commands.CannonRevolveSpin;
 import frc.robot.commands.DrivetrainArcadeDrive;
-import frc.robot.commands.RGBSetColor;
 import frc.robot.subsystems.Cannon;
 import frc.robot.subsystems.CannonAngleAdjust;
+import frc.robot.subsystems.CannonRevolve;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.tools.RGBController;
-import frc.robot.tools.RGBController.Color;
 
 public class RobotContainer {
   private XboxController mXbox = new XboxController(0);
   
   private final Cannon mCannon = new Cannon();
   private final CannonAngleAdjust mCannonAngleAdjust = new CannonAngleAdjust();
+  private final CannonRevolve mCannonRevolve = new CannonRevolve();
   private final Drivetrain mDrivetrain = new Drivetrain();
   private final RGBController mRGBController = new RGBController(new CANifier(Constants.CANIFIER));
 
-  private enum CommandSelector {
-    CANNON_RELOADING,
-    CANNON_FULL_FIRING_TANK,
-    CANNON_READY_TO_FIRE
-  }
-
   public RobotContainer() {
-    mCannon.setDefaultCommand(cannonReloadingCommands());
+    mCannon.setDefaultCommand(new CannonReloading(mCannon, mRGBController));
     mCannonAngleAdjust.setDefaultCommand(new CannonAimSetPercentOutputWithController(mCannonAngleAdjust, mXbox));
+    mCannonRevolve.setDefaultCommand(new CannonRevolveSetPercentOutput(mCannonRevolve, 0.0));
     mDrivetrain.setDefaultCommand(new DrivetrainArcadeDrive(mDrivetrain, mXbox));
 
     configureButtonBindings();
@@ -65,72 +55,12 @@ public class RobotContainer {
 		xButtonLeftStick = new JoystickButton(mXbox, 9);
     xButtonRightStick = new JoystickButton(mXbox, 10);
     
-    xButtonA.whenPressed(new ConditionalCommand(new ConditionalCommand(cannonFire(), new CannonRevolve(mCannon, 1, -0.4), () -> mCannon.getRevolveLimitSwitch()), new InstantCommand(), () -> mCannon.getFiringTankPressure() >= 75.0));
-    xButtonB.whenPressed(new CannonRevolve(mCannon, 8, 1.0));
-    xButtonX.whenPressed(new CannonRevolve(mCannon, 8, -1.0));
+    xButtonA.whenPressed(new ConditionalCommand(new ConditionalCommand(new CannonFire(mCannon, mCannonRevolve, mRGBController), new CannonRevolveSpin(mCannonRevolve, 1, -0.4), () -> mCannonRevolve.getRevolveLimitSwitch()), new InstantCommand(), () -> mCannon.getFiringTankPressure() >= 75.0));
+    xButtonB.whenPressed(new CannonRevolveSpin(mCannonRevolve, 8, 1.0));
+    xButtonX.whenPressed(new CannonRevolveSpin(mCannonRevolve, 8, -1.0));
 
-    xButtonLeftBumper.whenPressed(new CannonRevolve(mCannon, 1, -0.4));
-    xButtonRightBumper.whenPressed(new CannonRevolve(mCannon, 1, 0.4));
-  }
-
-  private Command cannonReloading() {
-    Command mCommand = new SequentialCommandGroup(
-      new ParallelRaceGroup(
-        new RGBSetColor(mRGBController, Color.Black),
-        new CannonFiringSolenoidSetState(mCannon, false).withTimeout(0.5)
-      ),
-      new CannonLoadingSolenoidSetState(mCannon, true).withTimeout(0.5)
-    );
-
-    return mCommand;
-  }
-
-  private Command cannonReadyToFire() {
-    return new RGBSetColor(mRGBController, Color.Red).withTimeout(0.1);
-  }
-
-  private Command cannonFullFiringTank() {
-    return new CannonLoadingSolenoidSetState(mCannon, false).withTimeout(0.5);
-  }
-
-  private CommandSelector cannonReloadingSelector() {
-    if (mCannon.getFiringTankPressure() <= 75.0) {
-      return CommandSelector.CANNON_RELOADING;
-    } else if (mCannon.getFiringTankPressure() >= 80.0) {
-      return CommandSelector.CANNON_FULL_FIRING_TANK;
-    } else {
-      return CommandSelector.CANNON_READY_TO_FIRE;
-    }
-  }
-
-  public Command cannonReloadingCommands() {
-    Command mCommand = new SelectCommand(
-      Map.ofEntries(
-        Map.entry(CommandSelector.CANNON_RELOADING, cannonReloading()),
-        Map.entry(CommandSelector.CANNON_FULL_FIRING_TANK, cannonFullFiringTank()),
-        Map.entry(CommandSelector.CANNON_READY_TO_FIRE, cannonReadyToFire())), 
-      this::cannonReloadingSelector);
-
-    return mCommand;
-  }
-
-  public Command cannonFire() {
-    Color[] redFlashing = {Color.Red, Color.Black};
-
-    Command mCommand = new SequentialCommandGroup(
-      new ParallelRaceGroup(
-        new RGBSetColor(mRGBController, redFlashing, 0.2),
-        new CannonLoadingSolenoidSetState(mCannon, false).withTimeout(1.0)
-      ),
-      new ParallelRaceGroup(
-        new RGBSetColor(mRGBController, Color.White),
-        new CannonFiringSolenoidSetState(mCannon, true).withTimeout(0.5)
-      ),
-      new RGBSetColor(mRGBController, Color.Black).withTimeout(0.5),
-      new CannonRevolve(mCannon, 1, 0.4)
-    );
-
-    return mCommand;
+    xButtonLeftBumper.whenPressed(new CannonRevolveSpin(mCannonRevolve, 1, -0.4));
+    xButtonRightBumper.whenPressed(new CannonRevolveSpin(mCannonRevolve, 1, 0.4));
   }
 
   public RGBController getRGBController() {
--- src/main/java/frc/robot/subsystems/Cannon.java
+++ src/main/java/frc/robot/subsystems/Cannon.java
@@ -1,26 +1,15 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Cannon extends SubsystemBase {
-  private final int CURRENT_LIMIT = 45;
-  private final double RAMP_RATE = 0.05;
-
-  private final CANSparkMax revolveMotor = new CANSparkMax(Constants.CANNON_REVOLVE_MOTOR, MotorType.kBrushed);
-
   private final Solenoid loadingSolenoid = new Solenoid(Constants.CANNON_LOADING_SOLENOID);
   private final Solenoid firingSolenoid = new Solenoid(Constants.CANNON_FIRING_SOLENOID);
-  private final DigitalInput revolveLimitSwitch = new DigitalInput(Constants.CANNON_REVOLVE_LIMIT_SWITCH);
 
   private final AnalogInput storagePressure = new AnalogInput(Constants.CANNON_STORAGE_PRESSURE);
   private final AnalogInput firingPressure = new AnalogInput(Constants.CANNON_FIRING_PRESSURE);
@@ -30,14 +19,7 @@ public class Cannon extends SubsystemBase {
   private final double SLOPE = 250.0;
   private final double Y_INTERCEPT = -25.0;
 
-  public Cannon() {
-    revolveMotor.restoreFactoryDefaults();
-    revolveMotor.setInverted(false);
-    revolveMotor.setIdleMode(IdleMode.kBrake);
-    revolveMotor.setSmartCurrentLimit(CURRENT_LIMIT);
-    revolveMotor.setOpenLoopRampRate(RAMP_RATE);
-    revolveMotor.burnFlash();
-  }
+  public Cannon() {}
 
   @Override
   public void periodic() {
@@ -53,16 +35,6 @@ public class Cannon extends SubsystemBase {
     SmartDashboard.putBoolean("Compressor Status", getCompressorStatus());
   }
 
-  public void setPercentOutput(double output) {
-    if (output > 1.0) {
-      output = 1.0;
-    } else if (output < -1.0) {
-      output = -1.0;
-    }
-
-    revolveMotor.set(output);
-  }
-
   public void setLoadingSolenoidState(boolean open) {
     loadingSolenoid.set(open);
   }
@@ -73,10 +45,6 @@ public class Cannon extends SubsystemBase {
 
   public boolean getLoadingSolenoidState() {
     return loadingSolenoid.get();
-  }
-
-  public boolean getRevolveLimitSwitch() {
-    return !revolveLimitSwitch.get();
   }
 
   public boolean getFiringSolenoidState() {
